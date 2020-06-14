4f8f1bc18fd73b076703f5a0838973d0162e40a3
nguyenethan01 <nguyenethan01@gmail.com>
2020-06-14 13:17:12 -0700

    add make_move mutator in board class

--- src/board.py
+++ src/board.py
@@ -4,4 +4,11 @@ class Board:
     def __init__(self,x:int,y:int):
         self.x = x
         self.y = y
-        self.array = [[0 for x in range(y)] for x in range(x)] 
+        self.array = [[-1 for x in range(y)] for x in range(x)] 
+
+    def make_move(self, x:int, y:int, currMove:int):
+        """
+            Given an x and y coordinate, move the knight to the given position and update array pos to currMove.
+        """
+        self.array[x][y] = currMove
+
