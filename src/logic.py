b26486785a5cc980ff1d1d62812ca3dd8a6988a9
nguyenethan01 <nguyenethan01@gmail.com>
2020-06-14 13:26:11 -0700

    remove isValid from logic file

--- src/logic.py
+++ src/logic.py
@@ -1,10 +1,16 @@
 #this is where all of the back-tracking logic of the knight's tour will reside
+import board
 
-def isValid(x,y,board:[[]]) -> bool:
-    """
-    Used to check if proposed move / position is valid on the given chess board
-    """
-    if(x >=0 and y>=0 and x<len(board[0]) and y<len(board)):
-        return True
-    return False
 
+def knightsTour(col,row,board:board.Board):
+    """
+    This function calculates the solution to the knight's tour given starting position (col,row) and a chess board
+    """
+    #these two arrays represent all possible moves a knight can make when both accessed with an index at the same time. For example, accessing index 0 would represent the move in which the x value is increased by two and the y value is increased by 1 
+    x_moves = [2,1,-1,-2,-2,-1,1,2]
+    y_moves = [1,2,2,1,-1,-2,-2,-1]
+
+
+
+    #set initial position (x,y) to 0
+    board.make_move()
