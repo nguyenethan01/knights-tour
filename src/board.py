4aec6d210819cddd4c4caf60eb870ccf180bc13e
nguyenethan01 <nguyenethan01@gmail.com>
2020-06-14 13:19:53 -0700

    add currMove attribute to board.py

--- src/board.py
+++ src/board.py
@@ -4,11 +4,13 @@ class Board:
     def __init__(self,x:int,y:int):
         self.x = x
         self.y = y
+        self.currMove = 0
         self.array = [[-1 for x in range(y)] for x in range(x)] 
 
-    def make_move(self, x:int, y:int, currMove:int):
+    def make_move(self, x:int, y:int):
         """
-            Given an x and y coordinate, move the knight to the given position and update array pos to currMove.
+            Given an x and y coordinate, move the knight to the given position and update array pos to currMove and increment currMove
         """
-        self.array[x][y] = currMove
+        self.array[x][y] = self.currMove
+        self.currMove+=1
 
