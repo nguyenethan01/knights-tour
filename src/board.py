f42f7821518ab036e5cd5c8e8f7d327d6f4f9cac
nguyenethan01 <nguyenethan01@gmail.com>
2020-06-14 13:25:42 -0700

    change naming schema and move isValid into board class

--- src/board.py
+++ src/board.py
@@ -1,16 +1,23 @@
 #this file holds the declaration of the board class, a way to modularize code
 
 class Board:
-    def __init__(self,x:int,y:int):
-        self.x = x
-        self.y = y
+    def __init__(self,col:int,row:int):
+        self.col = col
+        self.row = row
         self.currMove = 0
-        self.array = [[-1 for x in range(y)] for x in range(x)] 
+        self.array = [[-1 for x in range(row)] for x in range(col)] 
 
-    def make_move(self, x:int, y:int):
+    def make_move(self, col:int, row:int):
         """
-            Given an x and y coordinate, move the knight to the given position and update array pos to currMove and increment currMove
+            Given an col and row coordinate, move the knight to the given position and update array pos to currMove and increment currMove
         """
-        self.array[x][y] = self.currMove
+        self.array[col][row] = self.currMove
         self.currMove+=1
 
+    def isValid(self,col,row) -> bool:
+    """
+    Used to check if proposed move / position is valid in the given board dimensions
+    """
+    if(col >=0 and row>=0 and col<self.col and row<self.row):
+        return True
+    return False
