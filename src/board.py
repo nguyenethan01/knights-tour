b5e4cf35880c8d174fdc655e2a2b047433e1f822
nguyenethan01 <nguyenethan01@gmail.com>
2020-06-14 13:35:20 -0700

    add __str__ in board.py

--- src/board.py
+++ src/board.py
@@ -1,23 +1,33 @@
 #this file holds the declaration of the board class, a way to modularize code
 
 class Board:
-    def __init__(self,col:int,row:int):
-        self.col = col
+    def __init__(self,row:int,col:int):
         self.row = row
+        self.col = col
         self.currMove = 0
-        self.array = [[-1 for x in range(row)] for x in range(col)] 
+        self.array = [[-1 for x in range(col)] for x in range(row)] 
 
-    def make_move(self, col:int, row:int):
+    def __str__(self):
         """
-            Given an col and row coordinate, move the knight to the given position and update array pos to currMove and increment currMove
+            print string representation of current board
         """
-        self.array[col][row] = self.currMove
+        for i in range(self.row):
+            for j in range(self.col):
+                print(self.array[i][j],end = " ")
+            print()
+
+
+    def make_move(self, row:int, col:int):
+        """
+            Given an row and col coordinate, move the knight to the given position and update array pos to currMove and increment currMove
+        """
+        self.array[row][col] = self.currMove
         self.currMove+=1
 
-    def isValid(self,col,row) -> bool:
+    def isValid(self,row,col) -> bool:
     """
     Used to check if proposed move / position is valid in the given board dimensions
     """
-    if(col >=0 and row>=0 and col<self.col and row<self.row):
+    if(row >=0 and col>=0 and row<self.row and col<self.col):
         return True
     return False
