f0af06caffb1e0c5661fb3708378899d42e7c8f5
nguyenethan01 <nguyenethan01@gmail.com>
2020-06-14 14:50:49 -0700

    syntax fixes

--- src/run.py
+++ src/run.py
@@ -4,6 +4,6 @@ import logic
 
 
 
-if __name__ == "__main__"
-test = board.Board(8,8)
-logic.knights_tour(2,3,test)
+if __name__ == "__main__":
+    test = board.Board(8,8)
+    logic.knights_tour(2,3,test)
